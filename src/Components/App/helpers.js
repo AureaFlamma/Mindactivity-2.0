cfa525713bd9c0f07f64d6437877c184b68a5c93
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-18 13:35:46 +0000

    removed unused useState

--- src/Components/App/helpers.js
+++ src/Components/App/helpers.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import background1 from "../../assets/images/background_1.png";
 import background2 from "../../assets/images/background_2.png";
 import background3 from "../../assets/images/background_3.png";
@@ -16,7 +15,6 @@ import background4 from "../../assets/images/background_4.png";
 
 export function useRandomBackground() {
   var backgroundsArr = [background1, background2, background3, background4];
-  //ommiting setSelectedBackground as I'm not using it- otherwise Netlify will give me an error.
 
   return backgroundsArr[Math.floor(Math.random() * backgroundsArr.length)];
 }
