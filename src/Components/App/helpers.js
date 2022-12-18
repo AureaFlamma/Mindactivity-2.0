66b7dcc990f82669d6e2867c5d7bd6e1520fa5e2
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-18 13:31:32 +0000

    removed state from useRandomBackground

--- src/Components/App/helpers.js
+++ src/Components/App/helpers.js
@@ -4,12 +4,19 @@ import background2 from "../../assets/images/background_2.png";
 import background3 from "../../assets/images/background_3.png";
 import background4 from "../../assets/images/background_4.png";
 
+// export function useRandomBackground() {
+//   var backgroundsArr = [background1, background2, background3, background4];
+//   const [selectedBackground] = useState(RandomBackgroundURL(backgroundsArr)); //ommiting setSelectedBackground as I'm not using it- otherwise Netlify will give me an error.
+
+//   function RandomBackgroundURL(arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+//   }
+//   return selectedBackground;
+// }
+
 export function useRandomBackground() {
   var backgroundsArr = [background1, background2, background3, background4];
-  const [selectedBackground] = useState(RandomBackgroundURL(backgroundsArr)); //ommiting setSelectedBackground as I'm not using it- otherwise Netlify will give me an error.
+  //ommiting setSelectedBackground as I'm not using it- otherwise Netlify will give me an error.
 
-  function RandomBackgroundURL(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-  return selectedBackground;
+  return backgroundsArr[Math.floor(Math.random() * backgroundsArr.length)];
 }
